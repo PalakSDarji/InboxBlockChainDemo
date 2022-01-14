af1d2f45f5bb55beb7f18a2fea47bff22d770ec1
Palakkumar Darji <83695839+pdarji-mobiquityinc@users.noreply.github.com>
2022-01-14 15:31:22 +0530

    first commit

---
+++ contracts/Inbox.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.11;
+
+contract Inbox {
+
+  string public message;
+
+  constructor(string memory initMessage) {
+    message = initMessage;
+  }
+
+  function setMessage(string memory newMessage) public {
+    message = newMessage;
+  }
+}
---
+++ test/Inbox.test.js
@@ -0,0 +1,33 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());
+const compiled_contract = require('../compile');
+
+const interface = compiled_contract.output.contracts['Inbox.sol']['Inbox'].abi;
+const bytecode = compiled_contract.output.contracts['Inbox.sol']['Inbox'].evm.bytecode.object;
+
+let accounts;
+let inbox;
+
+beforeEach(async function(){
+
+  accounts = await web3.eth.getAccounts();
+
+  inbox = await new web3.eth.Contract(interface)
+    .deploy({ data : bytecode, arguments: ['Hi there!']})
+    .send({ from: accounts[0], gas: '1000000'});
+});
+
+
+describe('Inbox', function() {
+  it('deploys a contract', async function() {
+    assert.ok(inbox.options.address);
+  });
+});
+
+/*describe('', function(){
+  it('',async function() {
+
+  });
+});*/
